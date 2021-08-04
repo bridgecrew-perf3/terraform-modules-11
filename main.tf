71810e048bd36e7ab5f295ad399f73c962585a15
Graeson Nguyen <graeson@gmail.com>
2021-08-04 09:50:16 -0700

    Define environment variables

--- main.tf
+++ main.tf
@@ -1,15 +1,29 @@
 module "network" {
   source = "./network"
+  company = var.company
+  environment = var.environment
 }
 
 module "storage" {
   source = "./storage"
+  company = var.company
+  environment = var.environment
 }
 
 module "orders" {
   source = "./services/orders"
+  bucket = var.bucket
+  company = var.company
+  environment = var.environment
+  service_name = var.service_name
+  service_version = var.service_version
 }
 
 module "contracts" {
   source = "./services/contracts"
+  bucket = var.bucket
+  company = var.company
+  environment = var.environment
+  service_name = var.service_name
+  service_version = var.service_version
 }
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "bucket" {
+  type = string
+}
+
+variable "company" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "service_name" {
+  type = string
+}
+
+variable "service_version" {
+  type = string
+}
